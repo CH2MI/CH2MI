7e8c4115fff893df2fba0b3dd03ad024fa06bacf
CH2MI <gwanwoopa@naver.com>
2024-03-16 01:09:04 +0900

    Profile Init

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'lib',
+    version = '0.0.1',
+    description = 'profile test',
+    url = 'https://github.com/CH2MI/CH2MI',
+    author = 'CH2MI',
+    author_email = 'gwanwoopa@naver.com', 
+    license = 'CH2MI',
+    packages=find_packages()
+)
